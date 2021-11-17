5c197e6402aaffb10649ae01e71477bd1be016e7
katerina2018 <tvorchestvo15@gmail.com>
2021-11-17 19:25:08 +0200

    commit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,34 +1,41 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelectorAll('.js-close-menu');
+    const mobileMenu = document.querySelector(".js-menu-container");
+    const openMenuBtn = document.querySelector(".js-open-menu");
+    const closeMenuBtn = document.querySelectorAll(".js-close-menu");
 
     const toggleMenu = () => {
         const isMenuOpen =
-            openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-        mobileMenu.classList.toggle('is-open');
+            openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+        openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+        mobileMenu.classList.toggle("is-open");
 
-        // const scrollLockMethod = !isMenuOpen ?
-        //     'disableBodyScroll' :
-        //     'enableBodyScroll';
-        // bodyScrollLock[scrollLockMethod](document.body);
+        !isMenuOpen ? disableScrolling() : enableScrolling();
     };
 
-    openMenuBtn.addEventListener('click', toggleMenu);
-
+    openMenuBtn.addEventListener("click", toggleMenu);
 
     closeMenuBtn.forEach(function(item) {
-            item.addEventListener('click', toggleMenu);
-        })
-        // closeMenuBtn.addEventListener('click', toggleMenu);
-        // console.log(closeMenuBtn)
-        // Закрываем мобильное меню на более широких экранах
-        // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        item.addEventListener("click", toggleMenu);
+    });
+
+    // Закрываем мобильное меню на более широких экранах
+    // в случае изменения ориентации устройства.
+    window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
         if (!e.matches) return;
-        mobileMenu.classList.remove('is-open');
-        openMenuBtn.setAttribute('aria-expanded', false);
-        bodyScrollLock.enableBodyScroll(document.body);
+        mobileMenu.classList.remove("is-open");
+        openMenuBtn.setAttribute("aria-expanded", false);
+        enableScrolling();
     });
 })();
+
+function disableScrolling() {
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function() {
+        window.scrollTo(x, y);
+    };
+}
+
+function enableScrolling() {
+    window.onscroll = function() {};
+}
